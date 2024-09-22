b73896d1b642b4bd6a4ad5c6709b84de55d226aa
manosmin <manosmin@hotmail.com>
2024-09-22 18:25:05 +0300

    removed useless if from roleMiddleware

--- middlewares/roleMiddleware.js
+++ middlewares/roleMiddleware.js
@@ -1,9 +1,5 @@
 export const roleMiddleware = (...allowedRoles) => {
     return (req, res, next) => {
-      if (!req.user || !req.user.role) {
-        return res.status(403).json({ message: 'Error 403. User role not found.' });
-      }
-  
       if (!allowedRoles.includes(req.user.role)) {
         return res.status(403).json({ message: 'Error 403. You are not allowed to perform this action.' });
       }
